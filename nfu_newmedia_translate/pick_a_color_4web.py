e13e65c44a430a2fe425d201de0d92cea42ba222
Dahulin <756130867@qq.com>
2017-06-01 21:06:02 +0800

    修改
    修改了title

--- nfu_newmedia_translate/pick_a_color_4web.py
+++ nfu_newmedia_translate/pick_a_color_4web.py
@@ -15,7 +15,7 @@ def pick_a_words() -> 'html':
     """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
     user_words = request.form['user_words']	
     return render_template('results.html',
-                           the_title = '以下是您选取的颜色：',
+                           the_title = '以下是您的翻译结果：',
                            the_color = user_color,
                            )
 
