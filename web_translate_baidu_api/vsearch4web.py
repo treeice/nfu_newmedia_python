f635bedc51768ffeab9fa8ac041f3aea2bf93ab7
Wen-ha <641064738@qq.com>
2017-06-11 16:15:42 +0800

    网址参数连接

--- web_translate_baidu_api/vsearch4web.py
+++ web_translate_baidu_api/vsearch4web.py
@@ -31,7 +31,7 @@ def do_search() -> 'html':
 #网址参数连接
 	url_1 = 'http://api.fanyi.baidu.com/api/trans/vip/translate?'  
 	url_2 = 'q='+translate_q+'&from='+str(translate_from)+'&to='+str(translate_to)+'&appid='+str(translate_appid)+'&salt='+str
-(translate_appid)+'&salt='+str(translate_salt)+'&sign='+translate_sign_md5
+(translate_salt)+'&sign='+translate_sign_md5
 	url= url_1+url_2
 #网页打开  
 	r=requests.get(url)
